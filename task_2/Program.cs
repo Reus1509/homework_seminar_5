10a477efc91a59d355a31c65b24d0db5b9815216
Nikita Kuznetsov <nkuz1509@gmail.com>
2022-10-16 20:12:50 +0300

    add task_2

--- task_2/Program.cs
+++ task_2/Program.cs
@@ -1,2 +1,46 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте одномерный массив, заполненный случайными числами. 
+// Найдите сумму элементов, стоящих на нечётных позициях.
+
+// [3, 7, 23, 12] -> 19
+
+// [-4, -6, 89, 6] -> 0
+void Main()
+{
+    Console.Clear();
+    int[] array = new int[4];
+    FillArray(array);
+    WriteArray(array);
+    int result = OddSum(array);
+    Console.WriteLine($"Сумма нечетных чисел массива равна: {result}.");
+}
+
+void FillArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(-100,100);
+    }
+}
+
+void WriteArray(int[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+int OddSum(int[] array)
+{
+    int result = 0;
+    for (int i = 0; i < array.Length; i++)
+    {
+        if(array[i] % 2 != 0){
+            result += array[i];
+        }
+    }
+    return result;
+}
+
+Main();
