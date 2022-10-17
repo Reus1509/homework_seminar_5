acf9b969c94f1bb98a5440b30235f59d9ae120fe
Nikita Kuznetsov <nkuz1509@gmail.com>
2022-10-17 12:24:40 +0300

    add task_3

--- task_3/Program.cs
+++ task_3/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задайте массив вещественных чисел. 
+// Найдите разницу между максимальным и минимальным элементов массива.
+
+// [3 7 22 2 78] -> 76
+void Main(){
+    Console.Clear();
+    double[] array = new double[10];
+    FillArray(array);
+    WriteArray(array);
+    double result = FindMax(array) - FindMin(array);
+    Console.WriteLine($"максисмальное: {FindMax(array)}");
+    Console.WriteLine($"минимальное: {FindMin(array)}");
+    Console.Write($"Разница большего элемента и меньшего равна: {result}.");
+}
+
+void FillArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        array[i] = new Random().Next(0,100);
+    }
+}
+
+void WriteArray(double[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+double FindMin(double[] array)
+{   
+    double min = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] < min)
+    {
+        min = array[i];
+    }
+    }
+    return min;
+}
+
+double FindMax(double[] array)
+{   
+    double max = array[0];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i] > max)
+    {
+        max = array[i];
+    }
+    }
+    return max;
+}
+
+Main();
